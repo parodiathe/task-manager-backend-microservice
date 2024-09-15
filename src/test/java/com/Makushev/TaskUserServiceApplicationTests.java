a453b385d237f8988b63e0216b199e05d810a03e
Daniel M <parodiathe@mail.ru>
2024-09-15 20:58:55 +0300

    Initial commit

---
+++ src/test/java/com/Makushev/TaskUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Makushev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
